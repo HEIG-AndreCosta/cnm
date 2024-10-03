15905e9b886f77978521f5f906c4fd3160851765
André <andre_miguel_costa@hotmail.com>
2024-10-03 16:18:15 +0200

    feat: naive implementation test

---
+++ lab01_session01_matrix_multiplication/test.c
@@ -0,0 +1,53 @@
+#include <assert.h>
+#include <stdio.h>
+#include "matrix.h"
+
+#define M	    (3)
+#define N	    (4)
+#define K	    (2)
+#define RESULT_SIZE (M * N)
+
+// Used https://matrix.reshish.com/multCalculation.php to calculate
+int main(void)
+{
+	double a[K][M] = { { 1, 2, 3 }, { 4, 5, 6 } };
+	double b[N][K] = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+	double c[N][M];
+	double target[N][M] = {
+		{ 9, 12, 15 }, { 19, 26, 33 }, { 29, 40, 51 }, { 39, 54, 69 }
+	};
+
+	naive_matrix_multiplication((double *)a, (double *)b, (double *)c, M, N,
+				    K);
+
+	for (int i = 0; i < RESULT_SIZE; ++i) {
+		double ci = *((double *)c + i);
+		double targeti = *((double *)target + i);
+		assert(targeti == ci);
+	}
+	printf("Naive Matrix Multiplication OK");
+#if 0
+	puts("MATRIX A");
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 2; ++j) {
+			printf("%g ", a[j][i]);
+		}
+		printf("\n");
+	}
+	puts("MATRIX B");
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			printf("%g ", b[j][i]);
+		}
+		printf("\n");
+	}
+	puts("MATRIX C");
+
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			printf("%g ", c[j][i]);
+		}
+		printf("\n");
+	}
+#endif
+}
---
+++ lab01_session01_matrix_multiplication/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gcc -o test test.c matrix.c
+
+./test
