1ba5a806b3ad4ff4df39b2cdea680a92eea538b1
André <andre_miguel_costa@hotmail.com>
2024-10-06 10:54:31 +0200

    refactor: formatting

--- lab01_session01_matrix_multiplication/time-elapsed.py
+++ lab01_session01_matrix_multiplication/time-elapsed.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 import subprocess
 import argparse
-from tqdm import tqdm  # Importation de tqdm pour la barre de progression
+from tqdm import tqdm
 
 
 def main(start_size, end_size, increment, tile_size=None):
@@ -18,7 +18,11 @@ def main(start_size, end_size, increment, tile_size=None):
         print(f"Running the tiled implementation with tile size: {tile_size}")
         # Utilisation de tqdm pour afficher la progression
         outputs_tiling = [
-            float(subprocess.check_output(["./main", str(matrix_size), str(tile_size)]).decode())
+            float(
+                subprocess.check_output(
+                    ["./main", str(matrix_size), str(tile_size)]
+                ).decode()
+            )
             for matrix_size in tqdm(matrix_sizes, desc="Tiling Progress", unit="matrix")
         ]
         plt.plot(matrix_sizes, outputs_tiling, label="Tiling")
@@ -39,12 +43,20 @@ def main(start_size, end_size, increment, tile_size=None):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Matrix multiplication performance script.")
-    
-    parser.add_argument("-s", "--start", type=int, required=True, help="Start matrix size.")
+    parser = argparse.ArgumentParser(
+        description="Matrix multiplication performance script."
+    )
+
+    parser.add_argument(
+        "-s", "--start", type=int, required=True, help="Start matrix size."
+    )
     parser.add_argument("-e", "--end", type=int, required=True, help="End matrix size.")
-    parser.add_argument("-i", "--increment", type=int, required=True, help="Increment for matrix sizes.")
-    parser.add_argument("-t", "--tile", type=int, help="Tile size for tiled multiplication.")
+    parser.add_argument(
+        "-i", "--increment", type=int, required=True, help="Increment for matrix sizes."
+    )
+    parser.add_argument(
+        "-t", "--tile", type=int, help="Tile size for tiled multiplication."
+    )
 
     args = parser.parse_args()
 
