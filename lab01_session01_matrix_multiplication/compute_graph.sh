b7bfed5f0935ac2b6c3875a18735e8966331f509
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-10-10 16:06:54 +0200

    Work: edit script to have f(tile)

--- lab01_session01_matrix_multiplication/compute_graph.sh
+++ lab01_session01_matrix_multiplication/compute_graph.sh
@@ -4,12 +4,4 @@
 
 source /venv/bin/activate
 
-python3 perf.py -s 10 -e 1000 -i 10 -F naive10-1000
-python3 perf.py -s 10 -e 1000 -i 10 -t 2 -F tile10-1000-2
-python3 perf.py -s 10 -e 1000 -i 10 -t 4 -F tile10-1000-4
-python3 perf.py -s 10 -e 1000 -i 10 -t 10 -F tile10-1000-10
-python3 perf.py -s 10 -e 1000 -i 10 -t 20 -F tile10-1000-20
-python3 perf.py -s 10 -e 1000 -i 10 -t 50 -F tile10-1000-50
-python3 perf.py -s 10 -e 1000 -i 10 -t 100 -F tile10-1000-100
-python3 perf.py -s 10 -e 1000 -i 10 -t 200 -F tile10-1000-200
-python3 perf.py -s 10 -e 1000 -i 10 -t 500 -F tile10-1000-500
+python3 perf.py -s 0 -e 1024 -i 6 -F matrix-1024
