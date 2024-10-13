0df606728e92137b6ee4809a55edb1ad376e4996
André <andre_miguel_costa@hotmail.com>
2024-10-13 08:53:58 +0200

    feat: display data in readme table format directly

--- lab01_session02_convolution/perf.py
+++ lab01_session02_convolution/perf.py
@@ -12,29 +12,44 @@ def main():
     targets = [
         "edge_detection_no_unroll",
         "edge_detection_manual_unroll",
-        "edge_detection_compiler_unroll",
+        "edge_detection_compilero0_unroll",
+        "edge_detection_compilero1_unroll",
+        "edge_detection_compilero2_unroll",
     ]
     images = images_paths()
 
-    procs = [
-        {
-            "image": image,
-            "target": target,
-            "process": subprocess.Popen(
-                [f"./{target}", image],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-            ),
-        }
+    procs = {
+        image: [
+            {
+                "target": target,
+                "process": subprocess.Popen(
+                    [f"./{target}", image],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                ),
+            }
+            for target in targets
+        ]
         for image in images
-        for target in targets
-    ]
-    for process in procs:
-        output, _ = process["process"].communicate()
-        target = process["target"]
-        image = process["image"]
-        rows, cols, time = output.decode().split(", ")
-        print(f"{target} {image} rows: {rows} cols: {cols} time:{time}")
+    }
+    print(procs)
+
+    print(
+        "| Image  | Rows    | Columns | No Loop Unrolling | Conv. time (SW loop unrolling)| Conv. time (compiler -O0)| Conv. time (compiler -O1)| Conv. time (compiler -O2)|"
+    )
+    print(
+        "|--------|---------|---------|-------------------|-------------------------------|--------------------------|--------------------------|--------------------------|"
+    )
+    for image_name, processes in procs.items():
+
+        print(f"|{image_name}|")
+        for i, proc in enumerate(processes):
+            output, _ = proc["process"].communicate()
+            rows, cols, time = output.decode().split(", ")
+            if i == 0:
+                print(f"{rows}|{cols}|{time}|", end="")
+            else:
+                print(f"{time}|")
 
 
 if __name__ == "__main__":
