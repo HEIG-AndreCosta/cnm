f1fec58e596e19867702c060d166dc91dfedf794
André <andre_miguel_costa@hotmail.com>
2024-12-19 15:59:30 +0100

    feat: saxpy.cu

--- lab04_session02_cuda_thread_hierarchy/saxpy.cu
+++ lab04_session02_cuda_thread_hierarchy/saxpy.cu
@@ -6,70 +6,143 @@
 
 // TODO: CUDA kernel for scalar multiplication and vector addition, pay attention to stride
 
+__global__ void scalar_multiplication(size_t n, float a, float *x, float *y,
+				      float *z)
+{
+	size_t index = blockDim.x * blockIdx.x + threadIdx.x;
+	if (index < n) {
+		z[index] = a * x[index] + y[index];
+	}
+}
 // Scalar multiplication and vector addition
-void saxpy_cpu(int n, float a, float *x, float *y, float *z) {
-    for(int i = 0; i < n; ++i){
-        z[i] = a * x[i] + y[i];
-    }
+void saxpy_cpu(int n, float a, float *x, float *y, float *z)
+{
+	for (int i = 0; i < n; ++i) {
+		z[i] = a * x[i] + y[i];
+	}
 }
 
 // Check saxpy result
-bool check_saxpy(int n, float a, const float *x, const float *y, float *z) {
-   for(int i = 0; i < n; ++i) {
-        if(z[i] != a * x[i] + y[i]) return false;        
-    }
-    return true;
+bool check_saxpy(int n, float a, const float *x, const float *y, float *z)
+{
+	for (int i = 0; i < n; ++i) {
+		if (z[i] != a * x[i] + y[i])
+			return false;
+	}
+	return true;
 }
 
 int main(int argc, char const *argv[])
 {
-    int n = 1<<20; //2^20
-    float  a = 2;
+	int n = 1 << 20; //2^20
+	float a = 2;
 
-    // Allocate host memory
-    float *h_x = (float*)malloc(n*sizeof(float));
-    float *h_y = (float*)malloc(n*sizeof(float));
-    float *h_z = (float*)malloc(n*sizeof(float));
+	// Allocate host memory
+	float *h_x = (float *)malloc(n * sizeof(float));
+	float *h_y = (float *)malloc(n * sizeof(float));
+	float *h_z = (float *)malloc(n * sizeof(float));
 
-    if(h_x == NULL || h_y == NULL || h_z == NULL) {
-        printf("Error allocating host memory");
-        exit(EXIT_FAILURE);
-    }   
+	if (h_x == NULL || h_y == NULL || h_z == NULL) {
+		printf("Error allocating host memory");
+		exit(EXIT_FAILURE);
+	}
 
-    // Initialize input vectores
-    for(int i = 0; i < n; ++i) {
-        h_x[i] = 1.0;
-        h_y[i] = (float)(i);
-    }
+	// Initialize input vectores
+	for (int i = 0; i < n; ++i) {
+		h_x[i] = 1.0;
+		h_y[i] = (float)(i);
+	}
 
-    printf("Running SAXPY in CPU...\n");    
-    saxpy_cpu(n, a, h_x, h_y, h_z);
-    
-    //Check SAXPY CPU results
-    printf("Checking CPU SAXPY: %s\n", 
-        check_saxpy(n, a, h_x, h_y, h_z)? "Success": "Error");
-    
-    /* ********************************************************************* */
+	printf("Running SAXPY in CPU...\n");
+	saxpy_cpu(n, a, h_x, h_y, h_z);
 
-    //TODO: Device memory allocation and check for errors
-    
-    //TODO: Copy memory from host to device and check for errors
+	//Check SAXPY CPU results
+	printf("Checking CPU SAXPY: %s\n",
+	       check_saxpy(n, a, h_x, h_y, h_z) ? "Success" : "Error");
 
-    //TODO: Call kernel and check for errors
+	/* ********************************************************************* */
 
-    //TODO: Copy memory from device to host and check for errors
-    
-    //TODO: Free memory from device to host and check for errors
-    
-    /* ********************************************************************* */
-    
-    //Check SAXPY GPU results
-    printf("Checking GPU SAXPY: %s\n",
-        check_saxpy(n, a, h_x, h_y, h_z)? "Success": "Error");
+	const size_t size = n * sizeof(float);
+	float *d_x, *d_y, *d_z;
+	cudaError_t error = cudaMalloc((void **)&d_x, size);
+	if (error != cudaSuccess) {
+		return 1;
+	}
+	error = cudaMalloc((void **)&d_y, size);
+	if (error != cudaSuccess) {
+		cudaFree(d_x);
+		return 1;
+	}
+	error = cudaMalloc((void **)&d_z, size);
+	if (error != cudaSuccess) {
+		cudaFree(d_x);
+		cudaFree(d_y);
+		return 1;
+	}
 
-    free(h_x);
-    free(h_y);
-    free(h_z);
+	//TODO: Copy memory from host to device and check for errors
 
-    return 0;
+	error = cudaMemcpy(d_x, h_x, size, cudaMemcpyHostToDevice);
+	if (error != cudaSuccess) {
+		cudaFree(d_x);
+		cudaFree(d_y);
+		cudaFree(d_z);
+		return 1;
+	}
+	error = cudaMemcpy(d_y, h_y, size, cudaMemcpyHostToDevice);
+	if (error != cudaSuccess) {
+		cudaFree(d_x);
+		cudaFree(d_y);
+		cudaFree(d_z);
+		return 1;
+	}
+	error = cudaMemcpy(d_z, h_z, size, cudaMemcpyHostToDevice);
+	if (error != cudaSuccess) {
+		cudaFree(d_x);
+		cudaFree(d_y);
+		cudaFree(d_z);
+		return 1;
+	}
+	//TODO: Call kernel and check for errors
+
+	const size_t block_size = 256;
+	const size_t num_blocks = (n + block_size - 1) / block_size;
+
+	scalar_multiplication(n, a, d_x, d_y, d_z);
+
+	//TODO: Copy memory from device to host and check for errors
+
+	error = cudaMemcpy(h_z, d_z, size, cudaMemcpyDeviceToHost);
+	if (error != cudaSuccess) {
+		cudaFree(d_x);
+		cudaFree(d_y);
+		cudaFree(d_z);
+		return 1;
+	}
+
+	//TODO: Free memory from device to host and check for errors
+
+	error = cudaFree(d_x);
+	if (error != cudaSuccess) {
+		printf("Couldn't free dx\n");
+	}
+	error = cudaFree(d_y);
+	if (error != cudaSuccess) {
+		printf("Couldn't free dy\n");
+	}
+	error = cudaFree(d_z);
+	if (error != cudaSuccess) {
+		printf("Couldn't free dz\n");
+	}
+	/* ********************************************************************* */
+
+	//Check SAXPY GPU results
+	printf("Checking GPU SAXPY: %s\n",
+	       check_saxpy(n, a, h_x, h_y, h_z) ? "Success" : "Error");
+
+	free(h_x);
+	free(h_y);
+	free(h_z);
+
+	return 0;
 }
