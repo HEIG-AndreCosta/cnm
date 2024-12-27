61349999efc0b06f5eaf12a564546399cc73097e
André <andre_miguel_costa@hotmail.com>
2024-12-27 22:16:59 +0100

    feat: implement atomic increment

--- lab04_session03_memory_hierarchy/atomic.cu
+++ lab04_session03_memory_hierarchy/atomic.cu
@@ -4,54 +4,55 @@
 #define NUM_THREADS 10000000
 #define BLOCK_WIDTH 1000
 
-__global__ void increment(int * a, size_t size)
-{    
-    int i = blockIdx.x * blockDim.x + threadIdx.x;    
-    i = i % size;
-    a[i] = a[i] + 1;    
+__global__ void increment(int *a, size_t size)
+{
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	i = i % size;
+	atomicInc(a + i, 1);
 }
 
 // TODO implement increment kernel with CUDA atomics
 
 int main(int argc, char const *argv[])
 {
-    size_t size = 10;
+	size_t size = 10;
 
-    int *h_a = new int[size];
-    int *d_a;
+	int *h_a = new int[size];
+	int *d_a;
 
-    float event_elaspsed_time_ms = 0.0;
+	float event_elaspsed_time_ms = 0.0;
 
-    /* ********************************************************************* */
+	/* ********************************************************************* */
 
-    cudaEvent_t start, stop;
+	cudaEvent_t start, stop;
 
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
-    cudaMalloc((void**)&d_a,    size * sizeof(int));
-    cudaMemset((void*)  d_a, 0, size * sizeof(int));
+	cudaMalloc((void **)&d_a, size * sizeof(int));
+	cudaMemset((void *)d_a, 0, size * sizeof(int));
 
-    cudaEventRecord(start);
-    increment<<<std::ceil(NUM_THREADS/BLOCK_WIDTH), BLOCK_WIDTH>>>(d_a, size);
-    cudaEventRecord(stop);
+	cudaEventRecord(start);
+	increment<<<std::ceil(NUM_THREADS / BLOCK_WIDTH), BLOCK_WIDTH>>>(
+		d_a, size);
+	cudaEventRecord(stop);
 
-    cudaMemcpy(h_a, d_a, size * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_a, d_a, size * sizeof(int), cudaMemcpyDeviceToHost);
 
-    cudaEventElapsedTime(&event_elaspsed_time_ms, start, stop);
+	cudaEventElapsedTime(&event_elaspsed_time_ms, start, stop);
 
-    cudaFree(d_a);
+	cudaFree(d_a);
 
-    /* ********************************************************************* */
-    
-    printf("{");
-    for(size_t i = 0; i < size; ++i)
-        printf(" %d", h_a[i]);    
-    printf(" }\n");
-        
-    printf("%g ms\n", event_elaspsed_time_ms);
+	/* ********************************************************************* */
 
-    delete[] h_a;
+	printf("{");
+	for (size_t i = 0; i < size; ++i)
+		printf(" %d", h_a[i]);
+	printf(" }\n");
 
-    return 0;
+	printf("%g ms\n", event_elaspsed_time_ms);
+
+	delete[] h_a;
+
+	return 0;
 }
