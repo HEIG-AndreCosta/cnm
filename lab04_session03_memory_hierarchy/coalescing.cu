3ce849171e1761154d648893879ae3a4ed17c078
André <andre_miguel_costa@hotmail.com>
2024-12-28 11:52:03 +0100

    feat: complete coaslescing kernels

--- lab04_session03_memory_hierarchy/coalescing.cu
+++ lab04_session03_memory_hierarchy/coalescing.cu
@@ -1,59 +1,59 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-__global__ void kernel_number_3(float* a, int s)
+__global__ void kernel_number_3(float *a, int s)
 {
-  float e = 2.71828;
-  int i = blockDim.x * blockIdx.x + threadIdx.x;
-  //TODO implement access pattern number 3
+	float e = 2.71828;
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	a[i * s] = e;
 }
 
-__global__ void kernel_number_4(float* a, int s)
+__global__ void kernel_number_4(float *a, int s)
 {
-  float e = 2.71828;
-  int i = blockDim.x * blockIdx.x + threadIdx.x;
-  //TODO implement access pattern number 4
+	float e = 2.71828;
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	a[i + s] = e;
 }
 
 int main(int argc, char **argv)
 {
-  int n = 1<<20;
-  int blockSize = 256;
-  float time;
-  float *d_a;  
+	int n = 1 << 20;
+	int blockSize = 256;
+	float time;
+	float *d_a;
 
-  cudaEvent_t startEvent, stopEvent;
-    
-  cudaMalloc(&d_a, n * 32 * sizeof(float));
+	cudaEvent_t startEvent, stopEvent;
 
-  cudaEventCreate(&startEvent);
-  cudaEventCreate(&stopEvent);
+	cudaMalloc(&d_a, n * 32 * sizeof(float));
 
-  printf("Kernel access pattern number 3:\n");
-  for (int i = 0; i <= 32; i++) {
-    cudaEventRecord(startEvent,0);
-    kernel_number_3<<<n/blockSize, blockSize>>>(d_a, i);
-    cudaEventRecord(stopEvent,0);
+	cudaEventCreate(&startEvent);
+	cudaEventCreate(&stopEvent);
 
-    cudaEventSynchronize(stopEvent);
-    cudaEventElapsedTime(&time, startEvent, stopEvent);
+	printf("Kernel access pattern number 3:\n");
+	for (int i = 0; i <= 32; i++) {
+		cudaEventRecord(startEvent, 0);
+		kernel_number_3<<<n / blockSize, blockSize>>>(d_a, i);
+		cudaEventRecord(stopEvent, 0);
 
-    printf("%02d, %.5f ms\n", i, time);
-  }
+		cudaEventSynchronize(stopEvent);
+		cudaEventElapsedTime(&time, startEvent, stopEvent);
 
-  printf("Kernel access pattern number 4:\n");
-  for (int i = 1; i <= 32; i++) {
-    cudaEventRecord(startEvent, 0);
-    kernel_number_4<<<n/blockSize, blockSize>>>(d_a, i);
-    cudaEventRecord(stopEvent, 0);
+		printf("%02d, %.5f ms\n", i, time);
+	}
 
-    cudaEventSynchronize(stopEvent);
-    cudaEventElapsedTime(&time, startEvent, stopEvent);
+	printf("Kernel access pattern number 4:\n");
+	for (int i = 1; i <= 32; i++) {
+		cudaEventRecord(startEvent, 0);
+		kernel_number_4<<<n / blockSize, blockSize>>>(d_a, i);
+		cudaEventRecord(stopEvent, 0);
 
-    printf("%02d, %.5f ms\n", i, time);
-  }
+		cudaEventSynchronize(stopEvent);
+		cudaEventElapsedTime(&time, startEvent, stopEvent);
 
-  cudaEventDestroy(startEvent);
-  cudaEventDestroy(stopEvent);
-  cudaFree(d_a);
+		printf("%02d, %.5f ms\n", i, time);
+	}
+
+	cudaEventDestroy(startEvent);
+	cudaEventDestroy(stopEvent);
+	cudaFree(d_a);
 }
